cf559f6711778d8f24e3f4f6db92d2550382ab06
Andreas Kleber <andreaslangs@imac.lan>
2014-05-26 20:15:15 +0200

    separated sql schema and example

---
+++ example.sql
@@ -0,0 +1,15 @@
+BEGIN TRANSACTION;
+
+INSERT INTO "mails" VALUES(1,'2014-05-25 16:23:43.704887','Message-ID: <5381FCEF.1060407@nagios.com>
+Date: Sun, 25 May 2014 16:23:43 +0200
+From: nagios <nagios@nagios.com>
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:24.0) Gecko/20100101 Thunderbird/24.5.0
+MIME-Version: 1.0
+To: test@blub
+Subject: test
+Content-Type: text/plain; charset=ISO-8859-15; format=flowed
+Content-Transfer-Encoding: 7bit
+
+test');
+
+COMMIT;
